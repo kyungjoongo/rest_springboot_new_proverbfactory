30f58865545557bd2d70bf91ef1765693b640878
고경준 <kyungjoon.go@gmail.com>
2017-08-08 21:27:45 +0900

    getOne, getOneJson method add

--- src/main/java/com/kyungjoon/rest/rest/Constants.java
+++ src/main/java/com/kyungjoon/rest/rest/Constants.java
@@ -2,7 +2,7 @@ package com.kyungjoon.rest.rest;
 
 public class Constants {
 
-    public static String UPLOADED_FOLDER = "./upload/";
+    public static String UPLOADED_FOLDER = "./../upload/";
 
 
     /*public static String UPLOADED_FOLDER = "Z://upload_temp//";*/
